4d33f3187d4cc4e4cdf527b45ff4066a55d5b5af
arne <arne.vz@hotmail.be>
2021-05-05 23:10:46 +0200

    forwarding unit

--- RTL/forwarding_unit.v
+++ RTL/forwarding_unit.v
@@ -6,8 +6,8 @@ module forwarding_unit(
 		input wire [      4:0] register_rd_MEMWB,
       input wire [      4:0] register_addr1,
       input wire [      4:0] register_addr2,
-      input wire [      1:0] registrywrite_EXMEM,
-      input wire [      1:0] registrywrite_MEMWB,
+      input wire             registrywrite_EXMEM,
+      input wire             registrywrite_MEMWB,
 		output reg             forwardA,
       output reg             forwardB
    );
@@ -15,18 +15,26 @@ module forwarding_unit(
    always@(*)begin
       if((registrywrite_EXMEM == 1'b1) && (register_rd_EXMEM == register_addr1))begin
          forwardA = 1'b1;
+      end else begin
+         forwardA = 1'b0;
       end
          
       if((registrywrite_EXMEM == 1'b1) && (register_rd_EXMEM == register_addr2))begin
          forwardB = 1'b1;
+      end else begin
+         forwardB = 1'b0;
       end
 
       if((registrywrite_MEMWB == 1'b1) && (register_rd_MEMWB == register_addr1))begin
          forwardA = 1'b1;
+      end else begin
+         forwardB = 1'b0;
       end
          
       if((registrywrite_MEMWB == 1'b1) && (register_rd_MEMWB == register_addr2))begin
          forwardB = 1'b1;
+      end else begin
+         forwardB = 1'b0;
       end
 
    end
