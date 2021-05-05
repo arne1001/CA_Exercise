f6364a2284c80353436459829ca281c7a5f7dd82
arne <arne.vz@hotmail.be>
2021-05-05 22:43:02 +0200

    changed wire width

--- RTL/forwarding_unit.v
+++ RTL/forwarding_unit.v
@@ -6,10 +6,10 @@ module forwarding_unit(
 		input wire [      4:0] register_rd_MEMWB,
       input wire [      4:0] register_data1,
       input wire [      4:0] register_data2,
-      input wire [      2:0] registrywrite_EXMEM,
-      input wire [      2:0] registrywrite_MEMWB,
-		output reg [      2:0] forwardA,
-      output reg [      2:0] forwardB
+      input wire [      1:0] registrywrite_EXMEM,
+      input wire [      1:0] registrywrite_MEMWB,
+		output reg [      1:0] forwardA,
+      output reg [      1:0] forwardB
    );
    
    always@(*)begin
