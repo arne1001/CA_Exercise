4eaa2e7fee22e48ca8e287aac29904230a04c3b3
arne <arne.vz@hotmail.be>
2021-05-05 22:14:20 +0200

    fixed if statement forwarding unit

--- RTL/forwarding_unit.v
+++ RTL/forwarding_unit.v
@@ -1,30 +1,34 @@
 
 
 
-module forwarding_unit #(
-   	input wire [4:0] register_rd_EXMEM,
-		input wire [4:0] register_rd_MEMWB,
-      input wire [4:0] register_data1,
-      input wire [4:0] register_data2,
-      input wire [2:0] registrywrite_EXMEM,
-      input wire [2:0] registrywrite_MEMWB,
-		output reg [2:0] forwardA,
-      output reg [2:0] forwardB
+module forwarding_unit#(
+   	input wire [      4:0] register_rd_EXMEM,
+		input wire [      4:0] register_rd_MEMWB,
+      input wire [      4:0] register_data1,
+      input wire [      4:0] register_data2,
+      input wire [      2:0] registrywrite_EXMEM,
+      input wire [      2:0] registrywrite_MEMWB,
+		output reg [      2:0] forwardA,
+      output reg [      2:0] forwardB
 
    );
    
    always@(*)begin
-      if(registrywrite_EXMEM == 1 and register_rd_EXMEM == register_data1)
+      if(registrywrite_EXMEM == 1 and register_rd_EXMEM == register_data1) begin
          forwardA = 2
+      end
+         
 
-      if(registrywrite_EXMEM == 1 and register_rd_EXMEM == register_data2)
+      if(registrywrite_EXMEM == 1 and register_rd_EXMEM == register_data2) begin
          forwardB = 2
       end
 
-      if(registrywrite_MEMWB == 1 and register_rd_MEMWB == register_data1)
+      if(registrywrite_MEMWB == 1 and register_rd_MEMWB == register_data1) begin
          forwardA = 2
+      end
+         
 
-      if(registrywrite_MEMWB == 1 and register_rd_MEMWB == register_data2)
+      if(registrywrite_MEMWB == 1 and register_rd_MEMWB == register_data2) begin
          forwardB = 2
       end
 
